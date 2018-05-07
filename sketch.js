b00e10c75905835ba14d532499d6801495b13cf4
Pepijnroos <37802996+Pepijnroos@users.noreply.github.com>
2018-05-07 14:31:03 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -60,7 +60,7 @@ function setup(){
   }
 
   // Set values for data, and display the background image
-  image(stars, 0, 0,stars.width, stars.height);
+  image(stars, 0, 0, 1262, 921);
   time = 1664;
   startingframes = time * 60;
   speedoftime = 1;
@@ -71,7 +71,7 @@ function setup(){
 
 function draw(){
   // Draw background
-  image(stars, 0, 0,stars.width, stars.height);
+  image(stars, 0, 0, 1262, 921);
 
   // Update each ship
   for (i = 0; i < ships.length; i++){
