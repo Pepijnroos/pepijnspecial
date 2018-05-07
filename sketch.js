3d31be3349b331c834a4a68d7b472b67baa2a713
Pepijnroos <37802996+Pepijnroos@users.noreply.github.com>
2018-05-07 14:17:25 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function preload(){
 
 
 function setup(){
-  createCanvas(800,600);
+  createCanvas(800,700);
   for (i = 0; i < data.getRowCount(); i++){
     // get the data from table
     ship_id = data.getNum(i,0)
