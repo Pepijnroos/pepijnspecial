a00f357b94f93545d445f6943e8467d57cb339a8
Pepijnroos <37802996+Pepijnroos@users.noreply.github.com>
2018-05-24 11:37:06 +0200

    Update sketchfaden.js

--- sketchfaden.js
+++ sketchfaden.js
@@ -31,7 +31,7 @@ function preload(){
 
 
 function setup(){
-  createCanvas(924,802);
+  createCanvas(1429,955);
   for (i = 0; i < data.getRowCount(); i++){
     // get the data from table
     ship_id = data.getNum(i,0)
